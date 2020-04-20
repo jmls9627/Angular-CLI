cd4a4551e836a5a8c9734bd1b157a71b9439d7d1
jmls9627 <jmls9627@gmail.com>
2020-04-20 14:33:48 -0400

    Generating Code from Blueprints

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
+import { OrdersComponent } from './orders/orders.component';
+import { SearchBoxDirective } from './search-box.directive';
+import { InitCapsPipe } from './shared/init-caps.pipe';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent
+    CustomerComponent,
+    OrdersComponent,
+    SearchBoxDirective,
+    InitCapsPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
